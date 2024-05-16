20bea07a844ff51bdd8689d40b16572c135a0a33
Vidh3566 <Vidh3566@CSL0710.global.chainsys.com>
2024-05-16 11:13:01 +0530

    Working on servlet

--- DemoServletsApp/src/main/java/com/training/FirstServlet.java
+++ DemoServletsApp/src/main/java/com/training/FirstServlet.java
@@ -24,7 +24,7 @@ public class FirstServlet extends HttpServlet {
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 */ 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
